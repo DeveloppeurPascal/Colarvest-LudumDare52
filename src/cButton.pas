858b9826745fc4447044085ae34cf9f592dc2f07
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-04-07 10:15:06 +0200

    added copyright and signature comments

--- src/cButton.pas
+++ src/cButton.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Colarvest
+///
+/// Copyright 2023-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://colarvest.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Colarvest-LudumDare52
+///
+/// ***************************************************************************
+/// File last update : 2025-04-07T10:01:23.177+02:00
+/// Signature : a001b899ffecd64781a2459238e9c84d885fdd77
+/// ***************************************************************************
+/// </summary>
+
 unit cButton;
 
 interface
